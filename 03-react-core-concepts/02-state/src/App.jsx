72f53db815779c2e2f64e9d05a374e602373f0ad
developernahian <developer.nahian@gmail.com>
2024-10-22 08:58:17 +0600

    update

---
+++ 03-react-core-concepts/02-state/src/App.jsx
@@ -0,0 +1,52 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+
+    </>
+  )
+}
+
+export default App
+
+
+
+/*
+
+const {name, age} = {name: 'John', age: 30}
+
+
+
+
+const [first, second] = [1,2]
+
+
+
+
+
+function getSomething(){
+  return [1,2]
+}
+const [a,b] = getSomething()
+
+
+
+
+function useState(num){
+  function updateNum(value){
+    num = value;
+    return [num, updateNum]
+  }
+}
+const [num, updateNum] = useState(10)
+
+
+
+
+*/
